805946e2613c0916ed59ecd04c6507fce2ff76d9
James D <jamesgecko@gmail.com>
2020-11-03 18:20:31 -0500

    Stage 1

---
+++ app/main.rb
@@ -0,0 +1,48 @@
+def tick(args)
+  args.state.sprite_size ||= 16
+  args.state.tile_size ||= 72
+  args.state.num_tiles ||= 9
+  args.state.ui_width ||= 4
+
+  args.state.player.x ||= 0
+  args.state.player.y ||= 0
+
+  player_input(args)
+  args.outputs.solids << [0, 0, 1280, 720, 255, 255, 255]
+  args.outputs.sprites << player_draw(args)
+end
+
+def player_draw(args)
+  {
+    x: args.state.player.x,
+    y: args.state.player.y,
+    w: args.state.tile_size,
+    h: args.state.tile_size,
+    path: 'sprites.png',
+    tile_x: 0,
+    tile_y: 0,
+    tile_w: args.state.sprite_size,
+    tile_h: args.state.sprite_size
+  }
+end
+
+def player_move(args, delta_x, delta_y)
+  args.state.player.x += delta_x * args.state.tile_size
+  args.state.player.y += delta_y * args.state.tile_size
+end
+
+def player_input(args)
+  delta_x = 0
+  delta_y = 0
+  if args.inputs.controller_one.key_down.down  || args.inputs.keyboard.key_down.down
+    delta_y = -1
+  elsif args.inputs.controller_one.key_down.up  || args.inputs.keyboard.key_down.up
+    delta_y = 1
+  end
+  if args.inputs.controller_one.key_down.left  || args.inputs.keyboard.key_down.left
+    delta_x = -1
+  elsif args.inputs.controller_one.key_down.right  || args.inputs.keyboard.key_down.right
+    delta_x = 1
+  end
+  player_move(args, delta_x, delta_y)
+end
